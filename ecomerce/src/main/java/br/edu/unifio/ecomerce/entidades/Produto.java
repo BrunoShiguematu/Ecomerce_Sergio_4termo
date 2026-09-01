149b6b98c80168d45bd120ae96332c0e705294dd
BrunoShiguematu <bshiguematu@gmail.com>
2026-09-01 01:33:24 +0000

    Commit 4

---
+++ ecomerce/src/main/java/br/edu/unifio/ecomerce/entidades/Produto.java
@@ -0,0 +1,27 @@
+package br.edu.unifio.ecomerce.entidades;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Setter
+public class Produto {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Short estoque;
+    private BigDecimal preco;
+
+    @ManyToOne
+    private Categoria categoria;
+}
