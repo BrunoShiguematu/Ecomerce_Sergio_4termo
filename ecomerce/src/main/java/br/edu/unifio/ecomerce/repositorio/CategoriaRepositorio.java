aa9ff67f986875bcdd1847bde19a1cb59167b620
BrunoShiguematu <bshiguematu@gmail.com>
2026-09-22 01:03:37 +0000

    import sql

---
+++ ecomerce/src/main/java/br/edu/unifio/ecomerce/repositorio/CategoriaRepositorio.java
@@ -0,0 +1,9 @@
+package br.edu.unifio.ecomerce.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.unifio.ecomerce.entidades.Categoria;
+
+public interface CategoriaRepositorio extends JpaRepository<Categoria, Short> {
+
+}
